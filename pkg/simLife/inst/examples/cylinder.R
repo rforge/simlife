3f03aea6156a6c80b8593cf47eabc9d1ced75d29
bahama <bahama@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-08-27 21:51:02 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/simlife@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/simLife/inst/examples/cylinder.R
+++ pkg/simLife/inst/examples/cylinder.R
@@ -1,27 +1,28 @@
 \dontrun{
 
+library(rgl)
+library(unfoldr)
+
 ## Not on MS-Windows	
 #library("parallel")
-#options(mc.cores=detectCores())
-#options(parallel.option="mclapply")
+#options(mc.cores=2L)
 
 lam <- 35
 box <- list("xrange"=c(0,3),"yrange"=c(0,3),"zrange"=c(0,9))
 
 ## Spheroids of constant sizes
-theta <- list("size"=list(.5),
-		"shape"=list("radius"=0.1),
+theta <- list("size"=list(.5),"shape"=list("radius"=0.1),
 		"orientation"=list("kappa"=0.1))
 
 S <- simPoissonSystem(theta,lam,size="const",type="cylinders",
 		orientation="rbetaiso",box=box,pl=1,label="P")
 
 ## secondary phase: particles as spheres
-F <- simPoissonSystem(list(list("size"=0.075)),5, size="const",
+F <- simPoissonSystem(list("size"=list(0.075)), lam=5, size="const",
 		type="spheres",box=box, pl=1, label="F")
 
 ## apply RSA
-S2 <- rsa(S,box,F,pl=101,verbose=TRUE)
+S2 <- rsa(S,F,pl=101,verbose=TRUE)
 
 ## draw some projection
 #require("rgl")
@@ -29,14 +30,18 @@ S2 <- rsa(S,box,F,pl=101,verbose=TRUE)
 #cols <- c("#0000FF","#00FF00","#FF0000","#FF00FF","#FFFF00","#00FFFF")
 #cylinders3d(S2[id], box, col=cols)	
 #P <- getCylinderProjection(S2[id], B=c(0,1,0,1,1), draw=TRUE, conv = TRUE, np=20)
+#P <- getCylinderProjection(S2[id], B=c(0,0,0,0,0), draw=TRUE, conv = TRUE, np=20)
+#P <- getCylinderProjection(S2[id], B=c(1,1,1,1,1), draw=TRUE, conv = TRUE, np=20)
 
 ## construct clusters
-param <- list("r"=0.35)
-CLUST <- simCluster(S2, param, 0.1, box,verbose=TRUE)
+CL <- simPoissonSystem(list("size"=list(0.35)), lam=0.1, size="const",
+		type="spheres", box=box, pl=1, label="F")
+
+CLUST <- simCluster(S2, CL, cond=list("eps"=1e-7,"minSize"=1L), verbose=TRUE, pl=1)
 	
 ## densify
 ctrl <- list(threshold.stop=0.01,max.call=5000,verbose=FALSE)
-RET <- densifyCluster(S2, CLUST, box, ctrl, weight=10, cl = NULL)	
+RET <- densifyCluster(S2, CLUST, ctrl, weight=10, cl = NULL)	
 G <- RET$cluster
 
 ## draw original cluster
