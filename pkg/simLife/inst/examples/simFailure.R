dd8e65a60f5e5ea023f9825db2e9bf857379c31f
bahama <bahama@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-08-29 20:56:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/simlife@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/simLife/inst/examples/simFailure.R
+++ pkg/simLife/inst/examples/simFailure.R
@@ -2,7 +2,8 @@
 
 ## Simulate a defect accumulation
 ## of a particle system
-	
+library(simLife)
+
 ## primary particles (as prolate spheroids)
 ## and secondary phase (as spheres)
 data(AL2MC_20p_k10_F2p_S)
@@ -15,27 +16,45 @@ data(AL2MC_20p_k10_F2p_CL)
 box <- attr(S,"box")
 
 ## distTol=0.25, so use 25% of accumulation distance
-opt <- list("vickers"=107,"distTol"=0.25,
+opt <- list("vickers"=107, "distTol"=0.25, "Tmax"=10^12,
 		"inAreafactor"=1.56, "outAreafactor"=1.43, 
 		"pointsConvHull"=10, "scale"=1e+06,"pl"=101)
 
-par <- list("P"=c(0.01,10,0.5,80,-15,2),
-		"F"=c(0,0,0,105,-12,1),
-		"const"=NULL)
+## constants 'const' are set to the following internally
+## and can partially be overwritten if needed
+# const <- list("Em"=68.9,"Ef"=400,"nc"=28.2,"nu"=0.33,
+#		    "pf"=0.138,"nE"=NULL,"sigref"=276,"Vref"=5891)
+
+par <- list("P"=c(0.01,5,0.5,50,-11,3),
+		"F"=c(0,0,0,105,-12,1),"const"=NULL)
 
 # stress amplitude applied	
 stress <- 110
 ## generate individual (particles') failure times
 CLT <- simTimes(S,par,vickers=opt$vickers,stress=stress)
+
+## generated random failure times
+T <- unlist(sapply(CLT,`[[`,"T"))
+V <- unlist(sapply(CLT,`[[`,"V"))
+U <- unlist(sapply(CLT,`[[`,"U"))
+
+dev.new()
+showDensity(list("Debonding"=log10(V),"Fracture"=log10(U),"Failure time"=log10(T)),xlim=c(-2,15))
+
+# Area max
+(inA <- areaCrit(opt$vickers, stress=stress, factor=opt$inAreafactor, scale=opt$scale))
+(outA <- areaCrit(opt$vickers, stress=stress, factor=opt$outAreafactor, scale=opt$scale))
+
 ## run accumulation
 RET <- simDefect(stress,S,CLT,opt)
+length(RET)
 
-#### alternatively run
-#SIM <- simFracture(stress,S,opt,par,last.defect=FALSE,CL=CL)	
-#SIM$cl_info
-####
+#### alternatively, includes simulating times by 'simTimes'
+# SIM <- simFracture(stress,S,opt,par,last.defect=FALSE,CL=CL)	
+# SIM$cl_info
 
-## some simple analysis
+## some simple analysis of accumulation paths until failure,
+## that is, while the critical area is exceeded
 LR <- RET[[length(RET)]]
 isInCluster <- any(unlist(lapply(CL,function(x,y)
 					any(y$id %in% x$id) , y=LR)))
@@ -55,10 +74,11 @@ dev.new()
 L <- plotDefectAcc(RET,last.path=TRUE)
 
 ## 3D visualization of final defect projection area 
-# library(unfoldr)
-# qid <- LR$id
-# spheroids3d(S[qid],box=box, col=c("#0000FF","#00FF00","#FF0000","#FF00FF"))
+library(unfoldr)
+qid <- LR$id
+open3d()
+spheroids3d(S[qid],box=box, col=c("#0000FF","#00FF00","#FF0000","#FF00FF"))
 
 ## drawing only last cluster leading to failure
-# drawDefectProjections(S,list(LR))
+drawDefectProjections(S,list(LR))
 }
